deb7dcb8e4bb43e76dc77dcfa38a94246551caad
Juan Sebastian Lara <juanslara@MacBook-Air-de-Juan.local>
2025-07-03 19:22:15 +0900

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-b76d1a64-173d-4722-ac6d-d792aaf49fef.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
